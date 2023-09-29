8d3a55b39a9dbd068f14b39b25f88feec33a9e32
Rohith Mani varma <32290473+rohithmanivarma123@users.noreply.github.com>
2023-09-29 19:36:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ logging.basicConfig(level=logging.INFO)
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
